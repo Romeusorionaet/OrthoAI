0950b160536cf0ef902d0131640ba44e59b24f70
romeusorionaet <romeusoares14569@gmail.com>
2024-08-16 20:09:05 -0300

    feat: created header component

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header className="bg-cl_1 p-8 text-center">
+      <h1 className="text-4xl font-bold text-cl_4">
+        Ortho<span className="font-normal text-cl_3">AI</span>
+      </h1>
+    </header>
+  )
+}
