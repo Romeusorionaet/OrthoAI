c57774e20a8561ae9493626b56e9d45580c0f47b
romeusorionaet <romeusoares14569@gmail.com>
2024-08-30 16:02:02 -0300

    feat: organized components from home page

---
+++ src/app/(uploader)/page.tsx
@@ -0,0 +1,23 @@
+import { StepMarker } from '@/components/step-marker'
+import { FormUploader } from './components/form-uploader'
+
+export default function Home() {
+  return (
+    <main className="mx-auto w-full max-w-[1680px] space-y-10 px-4">
+      <StepMarker />
+
+      <div className="w-full max-w-[800px]">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, rerum!
+          Optio perferendis impedit exercitationem dolorem tempora amet sint ab
+          quasi mollitia vero, voluptatem id veritatis cupiditate? Perferendis
+          fugiat repellat dolores.
+        </p>
+      </div>
+
+      <section className="mx-auto flex w-full items-start gap-20 max-md:flex-col md:gap-4">
+        <FormUploader />
+      </section>
+    </main>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../assets/styles/globals.css'
+import '../assets/styles/scrollbar.css'
 import { Header } from '@/components/header'
 
 const inter = Inter({ subsets: ['latin'] })
